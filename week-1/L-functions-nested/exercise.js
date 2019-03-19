5bcb39c0815dfecb1157be601f43f45cf4d6f7fe
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-03-19 19:04:51 +0000

    changed L-function-nested

--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -12,22 +12,3 @@ console.log(createMessage(15, 8, "students"));
 console.log(createMessage(8, 15, "mentors"));
 
 //exercise 2
-function mentor(name) {
-  return name.toUpperCase();
-}
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
-
-function greeting() {
-  return "HELLO ";
-}
-
-console.log(greeting() + mentor(mentor1));
-console.log(greeting() + mentor(mentor2));
-console.log(greeting() + mentor(mentor3));
-console.log(greeting() + mentor(mentor4));
-console.log(greeting() + mentor(mentor5));
-
--- week-1/L-functions-nested/exercise2.js
+++ week-1/L-functions-nested/exercise2.js
@@ -1,5 +1,23 @@
+
+
+function mentor(name) {
+    return name.toUpperCase();
+}
 var mentor1 = "Daniel";
 var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function greeting(word) {
+    return word.toUpperCase();
+}
+
+var greetingToMentors =greeting("Hello ");
+
+console.log(greetingToMentors  +  mentor( mentor1));
+console.log(greetingToMentors + mentor(mentor2));
+console.log(greetingToMentors + mentor(mentor3));
+console.log(greetingToMentors+ mentor(mentor4));
+console.log(greetingToMentors + mentor(mentor5));
+
