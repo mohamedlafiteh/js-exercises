13afab7fffaa2d94ae223999b24847caec0ecc59
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-03-19 17:06:28 +0000

    Changed F-string file

--- week-1/F-strings-concatenation/exercise.js
+++ week-1/F-strings-concatenation/exercise.js
@@ -2,7 +2,7 @@
 // Start by creating a variable `message`
 var greetingfirst = "Hello";
 var message = "my name is";
-var name = "Mohamed";
+var name = " Mohamed";
 var greeting = greetingfirst + message + name;
 console.log(greeting);
 
